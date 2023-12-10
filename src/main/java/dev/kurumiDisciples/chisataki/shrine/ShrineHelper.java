772657e30ed7a9261d1cd878ae88b2e4434eb491
Hacking Pancakez <hackingpancakez@gmail.com>
2023-12-10 14:10:04 -0500

    Update ShrineHelper emojis

--- src/main/java/dev/kurumiDisciples/chisataki/shrine/ShrineHelper.java
+++ src/main/java/dev/kurumiDisciples/chisataki/shrine/ShrineHelper.java
@@ -3,6 +3,6 @@ package dev.kurumidisciples.chisataki.shrine;
 import dev.kurumidisciples.chisataki.enums.EmojiEnum;
 
 public class ShrineHelper {
-  public static EmojiEnum CHISATO_EMOJI = EmojiEnum.CHISATO_HEART;
-  public static EmojiEnum TAKINA_EMOJI = EmojiEnum.SAKANA;
+  public static EmojiEnum CHISATO_EMOJI = EmojiEnum.CHISATO_PADORU;
+  public static EmojiEnum TAKINA_EMOJI = EmojiEnum.TAKINA_PADORU;
 }
