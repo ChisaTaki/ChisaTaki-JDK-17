b430de16a51f5ddcd9a35e4e192ac8a38d56aabe
Hacking Pancakez <hackingpancakez@gmail.com>
2024-01-08 11:25:12 -0500

    Removed Christmas Command from CommandBuilder

--- src/main/java/dev/kurumidisciples/chisataki/commands/CommandBuilder.java
+++ src/main/java/dev/kurumidisciples/chisataki/commands/CommandBuilder.java
@@ -1,7 +1,20 @@
 package dev.kurumidisciples.chisataki.commands;
 
-import dev.kurumidisciples.chisataki.commands.slash.*;
-import dev.kurumidisciples.chisataki.commands.user.*;
+import dev.kurumidisciples.chisataki.commands.slash.DebugCommand;
+import dev.kurumidisciples.chisataki.commands.slash.EmbedCommand;
+import dev.kurumidisciples.chisataki.commands.slash.GifCommand;
+import dev.kurumidisciples.chisataki.commands.slash.IgnoreCommand;
+import dev.kurumidisciples.chisataki.commands.slash.MentionableCommand;
+import dev.kurumidisciples.chisataki.commands.slash.PercentageCommand;
+import dev.kurumidisciples.chisataki.commands.slash.RpsCommand;
+import dev.kurumidisciples.chisataki.commands.slash.SendRoleEmbedsCommand;
+import dev.kurumidisciples.chisataki.commands.slash.SendRulesEmbed;
+import dev.kurumidisciples.chisataki.commands.slash.ShrineCountCommand;
+import dev.kurumidisciples.chisataki.commands.slash.SlashCommand;
+import dev.kurumidisciples.chisataki.commands.slash.TTTCommand;
+import dev.kurumidisciples.chisataki.commands.slash.TestImageCommand;
+import dev.kurumidisciples.chisataki.commands.user.ToggleRoleCommand;
+import dev.kurumidisciples.chisataki.commands.user.UserCommand;
 import dev.kurumidisciples.chisataki.enums.RoleEnum;
 
 public class CommandBuilder {
@@ -47,7 +60,6 @@ public class CommandBuilder {
 				new SendRulesEmbed(),
 				new ShrineCountCommand(), 
 				new TestImageCommand(),
-				new ChristmasCommand(),
 				new TTTCommand()
 		};
 	}
