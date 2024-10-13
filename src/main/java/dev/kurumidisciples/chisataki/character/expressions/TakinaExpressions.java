d7f00758966688a6682dab4b3f31980da5d04340
Hacking-Pancakez <hackingpancakez@gmail.com>
2024-10-13 14:54:17 -0400

    refactor: Update TakinaExpressions to use new emoji for TAKINA_HUH

--- src/main/java/dev/kurumidisciples/chisataki/character/expressions/TakinaExpressions.java
+++ src/main/java/dev/kurumidisciples/chisataki/character/expressions/TakinaExpressions.java
@@ -8,7 +8,7 @@ public enum TakinaExpressions {
     TAKINA_HAPPY("tHappy", "<:derpeyes:1295096273577447485>"),
     TAKINA_BLUSH("tBlush", "<:takinablush:1294807910639206421>"),
     TAKINA_SAD("tSad", "<:takinacry:1294808353947783268>"),
-    TAKINA_HUH("tHuh", "<:TakinaProcessing:1014189860204073072>"),
+    TAKINA_HUH("tHuh", "<:processing:1295097202246418473>"),
     TAKINA_ANGRY("tAngry", "<:takinamad:1294808805816926228>"),
     TAKINA_CONFUSED("tConfused", "<:TakinaConcern:1044611069680832623>"),
     TAKINA_SMUG("tSmug", "<:TakinaSmug:1057655170886013018>"),
