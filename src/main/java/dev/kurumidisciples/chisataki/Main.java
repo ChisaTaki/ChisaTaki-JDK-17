6cee84a4304c382ff1604ccfba56b6d6f83c02c8
Hacking-Pancakez <hackingpancakez@gmail.com>
2026-03-02 11:05:58 -0500

    Removed release candidate tag from status message

--- src/main/java/dev/kurumidisciples/chisataki/Main.java
+++ src/main/java/dev/kurumidisciples/chisataki/Main.java
@@ -94,7 +94,7 @@ public class Main {
               commandCenter,
               new SantaInteraction()
           )
-          .setActivity(Activity.customStatus("On Honeymoon with Takina... (Release Candidate Ver)"))
+          .setActivity(Activity.customStatus("On Honeymoon with Takina..."))
           .build();
       jda.awaitReady(); // awaits for the cache system to build
       logger.info("Chisataki Bot successfully built and connected to JDA!");
